e3a51a42d2a327e29fedc8bb61afdf2dd84240d4
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-01-05 15:58:45 +0200

    .

--- src/felix86/v2/recompiler.cpp
+++ src/felix86/v2/recompiler.cpp
@@ -50,7 +50,9 @@ static void print_rdi(ThreadState* state) {
         u32 length = *(u32*)(rdi + 0x10);
         for (u32 i = 0; i < length; i++) {
             char c = *(char*)(rdi + 0x10 + 0x4 + i);
-            t += fmt::format("c: {} hex: {:x}", c, (int)c);
+            if (c != 0) {
+                t += c;
+            }
         }
         PLAIN("%s (len: %d)", t.c_str(), length);
     } else {
